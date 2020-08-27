dcec07c13257e7fc15d879e7b3f3548a59cfc92e
Ian Pyeatt <pyeatt08@gmail.com>
2020-08-27 18:30:00 -0500

    added create, update, delete burger functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("The Sandlot", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("The Kahuna Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Kahuna Burger", true);
